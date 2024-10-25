be3751264119c1f6347fd3136088d2c28c728434
Felipe Salmazo <felipesalmazo123@gmail.com>
2024-10-25 16:30:33 -0300

    API FINALIZADA

---
+++ devCarBE/Migrations/20241025120902_testenovidadeeee.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace devCarBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class testenovidadeeee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- devCarBE/Program.cs
+++ devCarBE/Program.cs
@@ -6,6 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DevCarDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Adiciona CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()   // Permite qualquer origem
+                   .AllowAnyMethod()   // Permite qualquer método (GET, POST, etc.)
+                   .AllowAnyHeader();  // Permite qualquer cabeçalho
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,6 +33,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Habilita CORS
+app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
 
